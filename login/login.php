2bff202cd8a1fc68f422b323a80f0e95d9dcfaa5
Crabi8 <101423267+Crabi8@users.noreply.github.com>
2022-04-14 13:01:05 +0200

    Connexion

--- login/login.php
+++ login/login.php
@@ -6,21 +6,50 @@
 </head>
 
 <body>
-    <div id="container">
-        <form>
+    <!-- <div id="container">
+        <form method="post">
             <h1>Connexion</h1>
 
-            <label for="nom_use"><b>Nom d'utilisateur</b></label>
-            <input type="text" placeholder="Entrer le nom d'utilisateur" id="nom_use" name="username" required>
+            <label for="connexion_nom"><b>Nom d'utilisateur</b></label>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" id="connexion_nom" name="username" required>
 
-            <label for="mdp"><b>Mot de passe</b></label>
-            <input type="password" placeholder="Entrer le mot de passe" id="mdp" name="password" required>
-            
+            <label for="connexion_mdp"><b>Mot de passe</b></label>
+            <input type="password" placeholder="Entrer le mot de passe" id="connexion_mdp" name="password" required>
+            <p></p>
             <label><b>Nouveau chez FootClub : <b><a style="color:#9D0208;" href="connexion.html">Veuillez créer un compte</a></label>
-
-            <input type="submit" id='submit' value='LOGIN'>
+            <p></p>
+            <input type="submit" id='submit' value='CONNEXION'>
 
         </form>
+    </div> -->
+    <div id="container">
+        <form method="post">
+            <h1>Inscription</h1>
+
+            <label for="inscription_nom"><b>Nom d'utilisateur</b></label>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" id="inscription_nom" name="username" required>
+
+            <label for="inscription_email"><b>Adresse email</b></label>
+            <input type="text" placeholder="Entrer votre adresse email" id="inscription_email" name="username" required>
+
+            <label for="inscription_mdp"><b>Mot de passe</b></label>
+            <input type="password" placeholder="Entrer le mot de passe" id="inscription_mdp" name="password" required>
+
+            <label for="inscription_confirmation_mdp"><b>Entrez le mot de passe à nouveau</b></label>
+            <input type="password" placeholder="Entrer à nouveau le mot de passe" id="inscription_confirmation_mdp" name="password" required>
+            <p></p>
+            <label><b>Vous avez déjà un compte : <b><a style="color:#9D0208;" href="#">Veuillez vous connecter</a></label>
+            <p></p>
+            <input type="submit" id='inscription_send' name='inscription_send' value='INSCRIPTION'>
+        </form>
+
+        <?php
+            if (isset($_POST['inscription_send'])){
+                $pseudo = $_POST['inscription_pseudo'];
+                $age = $_POST['inscription_age'];
+                $email = $_POST['inscription_email'];
+            }
+        ?>
     </div>
 </body>
 
