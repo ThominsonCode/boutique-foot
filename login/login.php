d39c8c122a40ff46f81d6897a76ae7485fc046c0
Crabi8 <101423267+Crabi8@users.noreply.github.com>
2022-04-15 08:24:59 +0200

    Fethi

--- login/login.php
+++ login/login.php
@@ -26,31 +26,62 @@
         <form method="post">
             <h1>Inscription</h1>
 
-            <label for="inscription_nom"><b>Nom d'utilisateur</b></label>
-            <input type="text" placeholder="Entrer le nom d'utilisateur" id="inscription_nom" name="username" required>
+            <!-- <label for="inscription_nom"><b>Nom d'utilisateur</b></label>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" id="inscription_nom" name="username"> -->
 
             <label for="inscription_email"><b>Adresse email</b></label>
-            <input type="text" placeholder="Entrer votre adresse email" id="inscription_email" name="username" required>
+            <input type="text" placeholder="Entrer votre adresse email" id="inscription_email" name="inscription_email">
 
             <label for="inscription_mdp"><b>Mot de passe</b></label>
-            <input type="password" placeholder="Entrer le mot de passe" id="inscription_mdp" name="password" required>
+            <input type="password" placeholder="Entrer le mot de passe" id="inscription_mdp" name="inscription_mdp">
 
             <label for="inscription_confirmation_mdp"><b>Entrez le mot de passe à nouveau</b></label>
-            <input type="password" placeholder="Entrer à nouveau le mot de passe" id="inscription_confirmation_mdp" name="password" required>
+            <input type="password" placeholder="Entrer à nouveau le mot de passe" id="inscription_confirmation_mdp" name="inscription_confirmation_mdp">
             <p></p>
             <label><b>Vous avez déjà un compte : <b><a style="color:#9D0208;" href="#">Veuillez vous connecter</a></label>
             <p></p>
             <input type="submit" id='inscription_send' name='inscription_send' value='INSCRIPTION'>
         </form>
-
-        <?php
-            if (isset($_POST['inscription_send'])){
-                $pseudo = $_POST['inscription_pseudo'];
-                $age = $_POST['inscription_age'];
-                $email = $_POST['inscription_email'];
-            }
-        ?>
     </div>
+
+    <?php
+
+    if (isset($_POST['inscription_send'])) {
+        extract($_POST);
+
+        if (!empty($inscription_mdp) && !empty($inscription_email) && !empty($inscription_confirmation_mdp)) {
+            if ($inscription_mdp == $inscription_confirmation_mdp) {
+                $options = [
+                    'cost' => 12,
+                ];
+                $hashpwd = password_hash($inscription_mdp, PASSWORD_BCRYPT, $options);
+
+                require 'database.php';
+
+                $db = Database::connect();
+
+                $statement = $db->prepare("SELECT mail FROM utilisateur WHERE email = :email");
+                $statement->execute(['email' => $inscription_email]);
+
+                $result = $statement->rowCount();
+
+                if ($result == 0) {
+                    $statement = $db->prepare("INSERT INTO utilisateur(mail, mot_de_passe) VALUES(:mail, :mot_de_passe)");
+                    $statement->execute([
+                        'mail' => $inscription_email,
+                        'mot_de_passe' => $hashpwd
+                    ]);
+                    echo 'Le compte a été créé';
+                }
+                else{
+                    echo 'Un email existe déjà';
+                }
+            }
+        } else {
+            echo "Les champs ne sont pas tous bien remplies";
+        }
+    }
+    ?>
 </body>
 
 </html>
