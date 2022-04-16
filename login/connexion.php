421a17e009bd37d5f17132305700ec01cdb5ce2c
Crabi8 <101423267+Crabi8@users.noreply.github.com>
2022-04-16 12:58:10 +0200

    Connexion

---
+++ login/connexion.php
@@ -0,0 +1,57 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <div id="container">
+        <form method="post">
+            <h1>Connexion</h1>
+
+            <label for="connexion_mail"><b>Adresse Mail</b></label>
+            <input type="text" placeholder="Entrer l'adresse mail" id="connexion_mail" name="connexion_mail" required>
+
+            <label for="connexion_mdp"><b>Mot de passe</b></label>
+            <input type="password" placeholder="Entrer le mot de passe" id="connexion_mdp" name="connexion_mdp" required>
+            <p></p>
+            <label><b>Nouveau chez FootClub : <b><a style="color:#9D0208;" href="inscription.php">Veuillez créer un compte</a></label>
+            <p></p>
+            <input type="submit" id='connexion_send' name='connexion_send' value='CONNEXION'>
+
+        </form>
+    </div>
+
+    <?php
+    if (isset($_POST['connexion_send'])) {
+        extract($_POST);
+
+        if (!empty($connexion_mdp) && !empty($connexion_mail)) {
+            // $options = [
+            //     'cost' => 12,
+            // ];
+            // $hashpwd = password_hash($connexion_mdp, PASSWORD_BCRYPT, $options);
+
+            require 'database.php';
+
+            $db = Database::connect();
+
+            $statement = $db->prepare("SELECT * FROM utilisateur WHERE mail = :email");
+            $statement->execute(['email' => $connexion_mail]);
+
+            $result =   $statement->fetch();
+
+            if (password_verify($connexion_mdp, $result['mot_de_passe'])) {
+                echo "Le mot de passe est bon, connection en cours";
+            } else {
+                echo "L'adresse mail portant l'email " . $connexion_mail . " n'existe pas !";
+            }
+        } else {
+            echo "Les champs ne sont pas tous bien remplies";
+        }
+    }
+    ?>
+</body>
+
+</html>
--- login/inscription.php
+++ login/inscription.php
@@ -6,28 +6,9 @@
 </head>
 
 <body>
-    <!-- <div id="container">
-        <form method="post">
-            <h1>Connexion</h1>
-
-            <label for="connexion_nom"><b>Nom d'utilisateur</b></label>
-            <input type="text" placeholder="Entrer le nom d'utilisateur" id="connexion_nom" name="username" required>
-
-            <label for="connexion_mdp"><b>Mot de passe</b></label>
-            <input type="password" placeholder="Entrer le mot de passe" id="connexion_mdp" name="password" required>
-            <p></p>
-            <label><b>Nouveau chez FootClub : <b><a style="color:#9D0208;" href="connexion.html">Veuillez créer un compte</a></label>
-            <p></p>
-            <input type="submit" id='submit' value='CONNEXION'>
-
-        </form>
-    </div> -->
     <div id="container">
         <form method="post">
             <h1>Inscription</h1>
-
-            <!-- <label for="inscription_nom"><b>Nom d'utilisateur</b></label>
-            <input type="text" placeholder="Entrer le nom d'utilisateur" id="inscription_nom" name="username"> -->
 
             <label for="inscription_email"><b>Adresse email</b></label>
             <input type="text" placeholder="Entrer votre adresse email" id="inscription_email" name="inscription_email">
@@ -38,7 +19,7 @@
             <label for="inscription_confirmation_mdp"><b>Entrez le mot de passe à nouveau</b></label>
             <input type="password" placeholder="Entrer à nouveau le mot de passe" id="inscription_confirmation_mdp" name="inscription_confirmation_mdp">
             <p></p>
-            <label><b>Vous avez déjà un compte : <b><a style="color:#9D0208;" href="#">Veuillez vous connecter</a></label>
+            <label><b>Vous avez déjà un compte : <b><a style="color:#9D0208;" href="connexion.php">Veuillez vous connecter</a></label>
             <p></p>
             <input type="submit" id='inscription_send' name='inscription_send' value='INSCRIPTION'>
         </form>
@@ -60,7 +41,7 @@
 
                 $db = Database::connect();
 
-                $statement = $db->prepare("SELECT mail FROM utilisateur WHERE email = :email");
+                $statement = $db->prepare("SELECT mail FROM utilisateur WHERE mail = :email");
                 $statement->execute(['email' => $inscription_email]);
 
                 $result = $statement->rowCount();
@@ -72,8 +53,7 @@
                         'mot_de_passe' => $hashpwd
                     ]);
                     echo 'Le compte a été créé';
-                }
-                else{
+                } else {
                     echo 'Un email existe déjà';
                 }
             }
@@ -83,5 +63,3 @@
     }
     ?>
 </body>
-
-</html>
