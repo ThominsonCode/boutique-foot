904b0adc67aea398541fcfcaa76abc9c95e810c9
Thomas Rocheteau <90977539+ThominsonCode@users.noreply.github.com>
2022-04-27 14:50:40 +0200

    essais libres

--- admin/index.php
+++ admin/index.php
@@ -19,6 +19,86 @@
 
 <body>
 
+
+    <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <img style="width: 100px;" href="#" src="../image/Logo_Club.png" alt="Logo_Club">
+        <!-- <a class="navbar-brand" href="#">LaBoutique</a> -->
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown-homme" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        HOMME
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">sous-cat 1</a>
+                        <a class="dropdown-item" href="#">sous-cat 2</a>
+                        <!-- <div class="dropdown-divider"></div> -->
+                        <a class="dropdown-item" href="#">sous-cat 3</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown-femme" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        FEMME
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">sous-cat 1</a>
+                        <a class="dropdown-item" href="#">sous-cat 2</a>
+                        <!-- <div class="dropdown-divider"></div> -->
+                        <a class="dropdown-item" href="#">sous-cat 3</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown-enfant" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        ENFANT
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">sous-cat 1</a>
+                        <a class="dropdown-item" href="#">sous-cat 2</a>
+                        <!-- <div class="dropdown-divider"></div> -->
+                        <a class="dropdown-item" href="#">sous-cat 3</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown-accessoires" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        ACCESSOIRES
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">sous-cat 1</a>
+                        <a class="dropdown-item" href="#">sous-cat 2</a>
+                        <!-- <div class="dropdown-divider"></div> -->
+                        <a class="dropdown-item" href="#">sous-cat 3</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown-nouveautes" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        NOUVEAUTES
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">sous-cat 1</a>
+                        <a class="dropdown-item" href="#">sous-cat 2</a>
+                        <!-- <div class="dropdown-divider"></div> -->
+                        <a class="dropdown-item" href="#">sous-cat 3</a>
+                    </div>
+                </li>
+            </ul>
+
+        </div>
+
+
+
+        <div class="nav-icons">
+            <a href="login/connexion.php"><i class="fas fa-user fa-2x"></i></a>
+            <a href="#"><i class="fas fa-cart-shopping fa-2x"></i></a>
+        </div>
+    </nav>
+
     <div class="container">
         <div class="row">
             <div class="col-12 col-sm-12 col-md-3">
@@ -32,6 +112,8 @@
                     <button data-toggle="list" href="#categorie-3" type="button" class="btn-admin"><span>Femmes</span></button>
 
                     <button href="#" type="button" class="btn-admin btn-admin-ajouter"><span>AJOUTER</span></button>
+                    <button href="#" type="button" class="btn-admin btn-admin-modifier"><span>MODIFIER</span></button>
+                    <button href="#" type="button" class="btn-admin btn-admin-supprimer"><span>SUPPRIMER</span></button>
                 </div>
 
                 <h4>Sous-catégories</h4>
@@ -45,6 +127,8 @@
                             <button data-toggle="list" href="#sous-categorie-2" type="button" class="btn-admin"><span>Sous-catégorie 2</span></button>
 
                             <button href="#" type="button" class="btn-admin btn-admin-ajouter"><span>AJOUTER</span></button>
+                            <button href="#" type="button" class="btn-admin btn-admin-modifier"><span>MODIFIER</span></button>
+                            <button href="#" type="button" class="btn-admin btn-admin-supprimer"><span>SUPPRIMER</span></button>
                         </div>
                     </div>
                     <div id="categorie-2" class="tab-pane fade">
@@ -53,6 +137,8 @@
                             <button data-toggle="list" href="#sous-categorie-4" type="button" class="btn-admin"><span>Sous-catégorie 4</span></button>
 
                             <button href="#" type="button" class="btn-admin btn-admin-ajouter"><span>AJOUTER</span></button>
+                            <button href="#" type="button" class="btn-admin btn-admin-modifier"><span>MODIFIER</span></button>
+                            <button href="#" type="button" class="btn-admin btn-admin-supprimer"><span>SUPPRIMER</span></button>
 
                         </div>
                     </div>
@@ -63,6 +149,8 @@
                             <button data-toggle="list" href="#sous-categorie-4" type="button" class="btn-admin"><span>Sous-catégorie 4</span></button>
 
                             <button href="#" type="button" class="btn-admin btn-admin-ajouter"><span>AJOUTER</span></button>
+                            <button href="#" type="button" class="btn-admin btn-admin-modifier"><span>MODIFIER</span></button>
+                            <button href="#" type="button" class="btn-admin btn-admin-supprimer"><span>SUPPRIMER</span></button>
 
                         </div>
                     </div>
