e313005b6ca26bcd2f5f34ea165b16a8cce732fe
2fast4u86 <104760709+2fast4u86@users.noreply.github.com>
2022-05-02 17:17:32 +0100

    Update panier.php

--- panier.php
+++ panier.php
@@ -5,8 +5,9 @@ require('common/header.php');
 require("common/navbar.php");
 ?>
 <h1>Votre panier</h1>
-<img src="chaussure1.png">
+<img src="Image/chaussure1.png">
 <h4>Prix: 79.90€</h4>
+<h6>Qte : 1</h6>
 <h3>Vans Les Limougeauds</h3>
 
 <p>Lubin</p>
