e9f6a1a103f6e1d68f2d74d232f90b8ac5371f3c
2fast4u86 <104760709+2fast4u86@users.noreply.github.com>
2022-05-02 10:40:26 +0100

    header panier

--- navbar.php
+++ navbar.php
@@ -91,6 +91,6 @@ if (!isset($_SESSION['user'])) {
                 ?>
             </div>
         </div>
-        <a href="panier/panier.htm"><i class="fas fa-cart-shopping fa-2x" style="color: black;"></i></a>
+        <a href="panier.php"><i class="fas fa-cart-shopping fa-2x" style="color: black;"></i></a>
     </div>
 </nav>
