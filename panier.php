a91f2fe55c98187083a41a9e89dfa44861468e9d
Crabi8 <101423267+Crabi8@users.noreply.github.com>
2022-05-02 18:26:11 +0200

    Update panier.php

--- panier.php
+++ panier.php
@@ -3,14 +3,30 @@ $css_sheet2 = "panierstyle";
 $css_sheet = "style";
 require('common/header.php');
 require("common/navbar.php");
+
+$statement = $db->query('SELECT * FROM categorie');
+$categories = $statement->fetchAll();
+$statement = $db->query('SELECT * FROM souscategorie');
+$sous_categories = $statement->fetchAll();
+$statement = $db->query('SELECT * FROM item');
+$items = $statement->fetchAll();
+
 ?>
+
 <h1>Votre panier</h1>
-<img src="image/chaussure1.png">
-<h4>Prix: 79.90€</h4>
-<h3>Vans Les Limougeauds</h3>
+<?php
+$statement = $db->query('SELECT * FROM item WHERE id = 25');
+$items = $statement->fetchAll();
+
+            foreach ($items as $item) {
+                echo '<img width="200px;" height="200px;" src="image/' . $item['image'] . '">
+                <h4>Prix: ' . number_format($item['prix'], 2, '.', '') . ' €</h4>
+                <h3>' . $item['nom'] . '</h3>';
+            }
+?>
 
 <p>Lubin</p>
 
 
-<?php require('common/footer.php') ?>
 
+<?php require('common/footer.php') ?>
